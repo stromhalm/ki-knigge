31ccd94f27231b0a7c0842242ff67431ea9799b0
Adrian Jagusch <adrian.jagusch@jumax.net>
2018-09-15 10:07:02 +0200

    Add player logic

--- src/app/ki-forum/ki-forum.component.ts
+++ src/app/ki-forum/ki-forum.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
@@ -13,27 +13,33 @@ export class KiForumComponent implements OnInit {
 
   commentsCollection: AngularFirestoreCollection<Comment>;
   comments: Observable<Comment[]>;
-  commentText: String;
+  commentText: string;
 
   constructor(
     public dialog: MatDialog,
     public db: AngularFirestore,
     public http: HttpClient
     ) {
-      this.commentsCollection = db.collection<Comment>('comments');
+      this.commentsCollection = db.collection<Comment>('comments', ref => ref.orderBy('created'));
       this.comments = this.commentsCollection.valueChanges();
     }
 
   async tryToPublishComment() {
 
     var response = await this.http.post<ApiResponse>('http://localhost:5000/api/', {input: this.commentText}).toPromise();
-    const dialogRef = this.dialog.open(VideoDialog, {maxWidth: '800px'});
 
-    dialogRef.afterClosed().subscribe(async result => {
+    if (parseInt(response.score) < 50) {
+      var dialogRef = this.dialog.open(VideoDialog, {width: '800px'});
+      var result = await dialogRef.afterClosed().toPromise();
       if (!result) return;
-      await this.commentsCollection.add({user: 'Anonymous', text: this.commentText});
-      this.commentText = '';
+    }
+    
+    this.commentsCollection.add({
+      user: 'Anonymous',
+      text: this.commentText,
+      created: new Date()
     });
+    this.commentText = '';
   }
 
   ngOnInit() {
@@ -41,26 +47,55 @@ export class KiForumComponent implements OnInit {
 }
 
 interface ApiResponse {
-  input: String,
-  cleanText: String,
-  score: String
+  input: string,
+  cleanText: string,
+  score: string,
 }
 
 interface Comment {
-  user: String,
-  text: String
+  user: string,
+  text: string,
+  created: Date
 }
 
 @Component({
   selector: 'dialog-dialog',
   templateUrl: 'video-dialog.html',
 })
-export class VideoDialog {
+export class VideoDialog implements OnInit {
 
-  constructor(
-    public dialogRef: MatDialogRef<VideoDialog>) {}
+  public videoCompleted: boolean = false;
+
+  constructor(public dialogRef: MatDialogRef<VideoDialog>) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  @HostListener('window:videoEvent', ['$event'])
+  videoEvent(event) {
+    switch(event.detail.name) {
+      case "complete":
+        this.videoCompleted = true;
+      break;
+    }
+  }
+
+  ngOnInit() {
+    var alertComponent = this;
+    window.alertMe = function(player, event, data) {
+      window.dispatchEvent(new CustomEvent('videoEvent', {detail: {player: player, name: event, data: data}}));
+    }
+    var _js3qi = setInterval(function () {
+      clearInterval(_js3qi);
+      var js3qVideoPlayer = new js3q({
+          'data-id': 'd556e477-b82f-11e8-ae4b-0cc47a188158',
+          'container': 'video-player',
+          'sticky': true,
+          'playlistbar' : true,
+          'jscallback' : 'alertMe'
+      });
+      js3qVideoPlayer.init();
+  }, 10);
   }
 }
